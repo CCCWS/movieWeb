6c5d65ec660d9506bbd16d3072138681aee16868
CCCWS <eung0626@gmail.com>
2022-04-23 03:44:15 +0900

    movie

--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -6,6 +6,8 @@ import { API_KEY, API_URL, IMG_URL } from "../config";
 import MovieHeader from "../components/Header/MovieHeader";
 import TitleLargeImg from "../components/TitleLargeImg";
 
+import "./Detail.css";
+
 function Detail() {
   const [movie, setMovie] = useState([]);
   const [loading, setLoading] = useState(true);
@@ -16,9 +18,11 @@ function Detail() {
   const infoRef = useRef();
   const storyRef = useRef();
 
-  const info = `${API_URL}movie/${id}?api_key=${API_KEY}&language=ko`;
-  const actor = `${API_URL}movie/${id}/credits?api_key=${API_KEY}`;
-  const reviews = `${API_URL}movie/${id}/reviews?api_key=${API_KEY}`;
+  const info = `${API_URL}movie/${id}?api_key=${API_KEY}&language=ko`; //영화 정보
+  const actor = `${API_URL}movie/${id}/credits?api_key=${API_KEY}&language=ko`; //영화 출연 배우
+  const reviews = `${API_URL}movie/${id}/reviews?api_key=${API_KEY}`; //영화 리뷰
+  const test1 = `${API_URL}person/${"배우id"}?api_key=${API_KEY}`; //배우 상세정보
+  const test2 = `${API_URL}person/${"배우id"}/credits?api_key=${API_KEY}`; //배우 출연작
 
   const getMovie = async () => {
     const res = await (await fetch(info)).json();
@@ -51,12 +55,68 @@ function Detail() {
         lookTitle={lookTitle}
       />
       {loading ? null : (
-        <div ref={titleRef}>
-          <TitleLargeImg IMG_URL={IMG_URL} {...movie} />
-        </div>
+        <>
+          <div ref={titleRef}>
+            <TitleLargeImg IMG_URL={IMG_URL} {...movie} />
+          </div>
+
+
+          
+          <hr />
+          <h1>d</h1>
+          <h1>d</h1>
+          <h1>d</h1>
+          <h1>d</h1>
+          <h1>d</h1>
+          <h1>d</h1>
+          <h1>d</h1>
+          <h1>d</h1>
+          <h1>d</h1>
+          <h1>d</h1>
+          <h1>d</h1>
+          <h1>d</h1>
+          <h1>d</h1>
+          <h1>d</h1>
+          <h1>d</h1>
+          <h1>d</h1>
+          <h1>d</h1>
+          <h1>d</h1>
+          <h1>d</h1>
+          <h1>d</h1>
+          <h1>d</h1>
+          <h1>d</h1>
+          <h1>d</h1>
+          <h1>d</h1>
+          <h1>d</h1>
+          <h1>d</h1>
+          <h1>d</h1>
+          <h1>d</h1>
+          <h1>d</h1>
+          <h1>d</h1>
+          <h1>d</h1>
+          <h1>d</h1>
+          <h1>d</h1>
+          <h1>d</h1>
+          <h1>d</h1>
+          <h1>d</h1>
+          <h1>d</h1>
+          <h1>d</h1>
+          <h1>d</h1>
+          <h1>d</h1>
+          <h1>d</h1>
+          <h1>d</h1>
+          <h1>d</h1>
+          <div className="productionLogo">
+            {movie.production_companies.map((data) => {
+              {
+                return data.logo_path === null ? null : (
+                  <img key={data.id} src={`${IMG_URL}w200${data.logo_path}`} />
+                );
+              }
+            })}
+          </div>
+        </>
       )}
-      <hr />
-      
     </>
   );
 }
