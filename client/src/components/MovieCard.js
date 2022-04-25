45ba5ec165199c5cbeaeb5ee87b354c45ccbcc56
CCCWS <eung0626@gmail.com>
2022-04-26 02:10:55 +0900

    movie

--- client/src/components/MovieCard.js
+++ client/src/components/MovieCard.js
@@ -1,29 +1,36 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
-
 import { SearchOutlined } from "@ant-design/icons";
+
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 function MovieCard({ id, poster_path, name, title, IMG_URL }) {
   const nav = useNavigate();
   const goDitail = () => {
     nav(`./detail/${id}`);
   };
+
+  useEffect(() => {
+    AOS.init();
+  });
+
   return (
-
-      <div className="movieCardItem" onClick={goDitail}>
-        <div className="movieCarePoster">
-          <div className="focusPoster">
-            <SearchOutlined />
-          </div>
-          <img src={`${IMG_URL}w200${poster_path}`} />
+    <div className="movieCardItem" onClick={goDitail}>
+      <div className="movieCarePoster">
+        <div className="focusPoster">
+          <SearchOutlined />
         </div>
 
-        <div className="movieCardTitle">
-          {title}
-          {name}
-        </div>
+        <img src={`${IMG_URL}w200${poster_path}`} />
       </div>
+
+      <div className="movieCardTitle">
+        {title}
+        {name}
+      </div>
+    </div>
   );
 }
 
