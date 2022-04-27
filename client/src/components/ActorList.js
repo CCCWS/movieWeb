716133664ace83980e2bc8a327d6f245f9b3dde1
CCCWS <eung0626@gmail.com>
2022-04-28 04:03:23 +0900

    movie

--- client/src/components/ActorList.js
+++ client/src/components/ActorList.js
@@ -2,31 +2,22 @@ import React from "react";
 import img from "../img/profile_none.PNG";
 
 function ActorList({ movieActor, IMG_URL }) {
+  const test = movieActor.slice(0, 8);
+
   return (
     <>
       <div className="actor">
         {movieActor.map((data) => (
           <div className="actorOne" key={data.id}>
-            {data.profile_path === null ? (
-              <>
-                <div
-                  className="actorImg"
-                  style={{
-                    backgroundImage: `url(${img})`,
-                  }}
-                />
-              </>
-            ) : (
-              <>
-                <div
-                  className="actorImg"
-                  style={{
-                    backgroundImage: `url(${IMG_URL}w200${data.profile_path})`,
-                  }}
-                />
-              </>
-            )}
-
+            <div
+              className="actorImg"
+              style={{
+                backgroundImage:
+                  data.profile_path === null
+                    ? `url(${img})`
+                    : `url(${IMG_URL}w200${data.profile_path})`,
+              }}
+            ></div>
             <div className="actorName">
               {data.name}
               <br />
